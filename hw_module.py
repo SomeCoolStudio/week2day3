a401a47d3cdc2a400d94e2ce4e086b6e9394b59a
SomeCoolStudio <47907012+SomeCoolStudio@users.noreply.github.com>
2024-02-29 11:51:35 -0800

    Week 2 Day 3

---
+++ hw_module.py
@@ -0,0 +1,11 @@
+
+def calculate_house_square_footage (length, width):
+
+    return print ("The square footage of your home is: ", round(length * width, 2))
+
+
+
+def calculate_circumference_of_cirlce (radius):
+
+    return print ("The circumference of this circle is: ", round(2 * 3.14 * radius, 2))
+
